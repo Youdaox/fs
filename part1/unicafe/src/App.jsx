859971cb964b7b9dd836586f5ad8ac897538ab5d
Youdao Xing <Youdaox>
2025-07-18 14:34:59 +1200

    part2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,53 +1,55 @@
-import { useState } from 'react'
-
-const Heading = (props) => (
-  <h1> {props.text} </h1>
-)
-
-const StatisticLine = (props) => (
-  <tr>
-    <td>{props.text}</td> 
-    <td>{props.value}</td>
-  </tr>
-)
-const Button = (props) => (
-  <button onClick={props.onClick}> {props.text}</button>
-)
-
-const Statistics = ({good,bad,neutral}) => {
-  if (good + bad + neutral == 0){
-    return (
-      <div>No feedback given</div>
-    )
-  }
-  return (
-  <table>
-    <tbody>
-      <StatisticLine text ="good" value ={good}/>
-      <StatisticLine text ="neutral" value ={neutral}/>
-      <StatisticLine text ="bad" value ={bad}/>
-      <StatisticLine text ="all" value ={good + bad + neutral}/>
-      <StatisticLine text ="average" value ={good + bad + neutral / 3}/>
-      <StatisticLine text ="positive" value ={good / (good + bad + neutral) * 100 + " %"}/>
-    </tbody>
-  </table>
-  )
-}
+import Course from './components/Course'
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <>
-      <Heading text="give feedback" />
-      <Button onClick={() => setGood(good +1)} text="good" />
-      <Button onClick={() => setNeutral(neutral +1)}text="neutral" />
-      <Button onClick={() => setBad(bad +1)}text="bad" />
-      <Heading text="statistics" />
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-    </>
+  <>
+    <h1>Web Development curriculum</h1>
+    {courses.map(course => <Course course={course} />)}
+  </>
   )
 }
 
