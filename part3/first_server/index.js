f639f7012dd10fde8f9c9c2e21944e2c7321d37c
Youdao Xing <Youdaox>
2025-08-04 01:48:16 +1200

    created database for phonebook app

--- part3/first_server/index.js
+++ part3/first_server/index.js
@@ -1,81 +1,71 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.static('dist'))
 app.use(express.json())
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>notes backend</h1>')
 })
 
 app.get('/api/notes', (req,res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+      res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
     const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
-    }
+    Note.findById(id).then(returnedNote => {
+      res.json(returnedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id
-    notes = notes.filter(note => (note.id !== id))
-
-    res.status(204).end()
+    Note.findByIdAndDelete(id).then(res.status(204).end())
 })
 
-const generateId = () => {
-  const id = (notes.length > 0) ? Math.max(...notes.map(n => Number(n.id))) : 0
-  return id+1
-}
 app.post('/api/notes', (req, res) => {
   const body = req.body
 
   if (!body.content) {
     return res.status(400).json({error: 'content-missing'})
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
   
-  notes = notes.concat(note)
-
-  res.json(note)
-  console.log(note);
-  
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  })
 })
 
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  
+  const body = req.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  Note.findByIdAndDelete(id).then(
+    note.save().then(savedNote => {
+      res.json(savedNote)
+    })
+  )
+})
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
 })
--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,31 +1,59 @@
 const mongoose = require('mongoose')
 
+const login = async (password) => {
+  const url = `mongodb+srv://youdao:${password}@cluster0.woqbz63.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+  mongoose.set('strictQuery',false)
+
+  await mongoose.connect(url)
+}
+
+const createPerson = (name, number) => {
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+  const person = new Person({
+    "name": name,
+    "number": number,
+  })
+
+  person.save().then(res => {
+    console.log(res);
+    mongoose.connection.close()
+  })
+}
+
+const printAll = async (password) => {
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: String
+  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+  Person.find({}).then(res => {
+    console.log("phonebook:");
+    res.forEach(person => {
+      console.log(person.name, person.number);
+    })
+     mongoose.connection.close()
+  })
+}
+
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
+} else if (process.argv.length < 4) {
+  login(process.argv[2])
+  printAll()
+} else if (process.argv.length < 5) {
+  console.log('missing name or number as argument')
+} else {
+  login(process.argv[2])
+  createPerson(process.argv[3], process.argv[4])
 }
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://youdao:${password}@cluster0.woqbz63.mongodb.net/PhonebookApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
