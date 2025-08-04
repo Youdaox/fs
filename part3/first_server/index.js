b9729725d828658de5e544d570825cc02e85e7a2
Youdao Xing <Youdaox>
2025-08-04 21:27:49 +1200

    exercise 3.18

--- part3/first_server/index.js
+++ part3/first_server/index.js
@@ -10,25 +10,35 @@ app.get('/', (req, res) => {
   res.send('<h1>notes backend</h1>')
 })
 
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
     Note.find({}).then(notes => {
       res.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
     const id = req.params.id
-    Note.findById(id).then(returnedNote => {
-      res.json(returnedNote)
+    Note.findById(id)
+    .then(returnedNote => {
+      if (returnedNote){
+        res.json(returnedNote)
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res, next) => {
     const id = req.params.id
-    Note.findByIdAndDelete(id).then(res.status(204).end())
+    Note.findByIdAndDelete(id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
 
   if (!body.content) {
@@ -44,27 +54,43 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.put('/api/notes/:id', (req, res) => {
+app.put('/api/notes/:id', (req, res, next) => {
   const id = req.params.id
   
   const body = req.body
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
+  Note.findById(id)
+  .then(note => {
+    if (!note) {
+      return res.status(404).end()
+    } 
+    note.content = body.content
+    note.important = body.important
 
-  Note.findByIdAndDelete(id).then(
-    note.save().then(savedNote => {
-      res.json(savedNote)
+    return note.save().then(updatedNote => {
+      res.json(updatedNote)
     })
-  )
+  })
+  .catch(error => next(error))
 })
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -10,33 +10,38 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[Content-Length] :response-time ms :data'))
 
+
+
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(notes => {
       res.json(notes)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id',(req, res) => {
+app.get('/api/persons/:id',(req, res, next) => {
     const id = req.params.id
     
     Person.findById(id)
       .then(person => {
+        if (!person) {
+          return res.status(404).end()
+        }
         res.json(person)
       })
-      .catch(error => {
-        res.send(`<h1>${id} does not exist</h1>`)
-      })
+      .catch(error => next(error))
 })
 
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
     Person.findByIdAndDelete(id).then(result => {
       res.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number){
@@ -50,9 +55,10 @@ app.post('/api/persons', (req, res) => {
     person.save().then(newPerson => {
       res.json(newPerson)
     })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const id = req.params.id
 
   const body = req.body
@@ -60,24 +66,35 @@ app.put('/api/persons/:id', (req, res) => {
   if (!body.name || !body.number){
       return res.status(400).send({ error: 'missing name or number' })
   }
-  const person = new Person({
-    "name": body.name, 
-    "number": body.number
-  })
 
-  Person.findByIdAndDelete(id)
-    .then(
-      person.save().then(updatedPerson => {
-        res.json(updatedPerson)
-      })
-    )
-    .catch(error => {
-      res.send(`<h1>${id} does not exist</h1>`)
+  Person.findById(id)
+    .then(result => {
+      if (!result){
+        return res.status(404).end()
+      }
+      result.name = body.name
+      result.number = body.number
+      result.save()
+        .then(updatedPerson => {
+          res.json(updatedPerson)
+        })
     })
+    .catch(error => next(error))
 
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
