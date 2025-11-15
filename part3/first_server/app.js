e7130f80f01496efd223579feac37627ff872c87
Youdaox <youdao005@gmail.com>
2025-11-16 00:20:05 +1300

    implemented frontend for backend features

--- part3/first_server/app.js
+++ part3/first_server/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const cors = require('cors')
 
 const app = express()
 
@@ -20,6 +21,7 @@ mongoose
     logger.error('error connection to MongoDB:', error.message)
   })
 
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
